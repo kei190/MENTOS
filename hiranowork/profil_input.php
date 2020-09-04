f2a17bfc64f1952dfc6dc09041244e9f56df30fa
kei190 <nogi.121126@gmail.com>
2020-09-04 16:45:13 +0900

    2020/09/04 プロフィール入力画面　平野

--- hiranowork/profil_input.php
+++ hiranowork/profil_input.php
@@ -2,13 +2,19 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>プロフィール作成画面</title>
+<title>コロナで出会っちゃった!? てへっ☆</title>
 </head>
 <body>
-<label>プロフィール作成</label>
-<p>ユーザ名：<input type="text" name="user_name"></p>
-<p>生年月日：<input type="date" name="day"></p>
-<p>一言：<input type="text" name="message"></p>
-<p><input type="submit" value="作成"></p>
+<header>
+<h3 style="text-align: right">コロナで出会っちゃった!? てへっ☆</h3>
+</header>
+<div style="text-align: center">
+    <h2>プロフィール作成</h2>
+    <p>ユーザ名：<input type="text" name="user_name"></p>
+    <p>生年月日：<input type="date" name="day"></p>
+    <p>一言：<input type="text" name="message" style="height: 80px"></p>
+    <p><input type="submit" value="作成"></p>
+</div>
+
 </body>
 </html>
