9d71f5bf2d5d7f849ea56b5fbd5d1eb1e0bdbe7e
kei190 <nogi.121126@gmail.com>
2020-09-04 14:18:22 +0900

    2020/09/04 プロフィール入力画面　平野

---
+++ hiranowork/profil_input.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>プロフィール作成画面</title>
+</head>
+<body>
+<label>プロフィール作成</label>
+<p>ユーザ名：<input type="text" name="user_name"></p>
+<p>生年月日：<input type="date" name="day"></p>
+<p>一言：<input type="text" name="message"></p>
+<p><input type="submit" value="作成"></p>
+</body>
+</html>
